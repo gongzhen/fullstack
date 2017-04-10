e3cc452e662aac4621219cadcb8eceaa3458a073
gongzhen <gongzhengz@gmail.com>
2017-04-10 14:42:49 -0700

    2.2-end

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-import config, {nodeEnv, logStars} from './config';
+import http from 'http';
 
-console.log(config, nodeEnv, logStars);
+const server = http.createServer((req, res) => {
+    res.write('Hello Http!\n');
+    setTimeout(() => {
+        res.write('It works\n');
+        res.end();        
+    }, 3000);    
+});
 
-logStars('function')
+server.listen(8080);
