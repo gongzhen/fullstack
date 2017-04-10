08c15e78dd6d643c4f4534a24fa36af1783453a4
gongzhen <gongzhengz@gmail.com>
2017-04-10 15:24:48 -0700

    2.4-end

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-import http from 'http';
+import config from './config';
+import apiRouter from './api';
+// import fs from 'fs';
 
-const server = http.createServer((req, res) => {
-    res.write('Hello Http!\n');
-    setTimeout(() => {
-        res.write('It works\n');
-        res.end();        
-    }, 3000);    
+import express from 'express';
+const server = express();
+
+// curl http://localhost:8080/
+// output: Hello Express
+server.get('/', (req, res) => {
+    res.send('Hello Express');
 });
 
-server.listen(8080);
+server.use('/api', apiRouter);
+
+server.use(express.static('public'));
+
+// curl http://localhost:8080/about.html
+// output: About
+// server.get('/about.html', (req, res) => {
+//     fs.readFile('./about.html', (err, data) => {
+//         res.send(data.toString());
+//     });
+// });
+
+server.listen(config.port, ()=>{
+    console.info('Express listening on port', config.port);
+});
