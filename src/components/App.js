e9e594bd9d17a265b0b5318a43c769fb6c37b9a6
gongzhen <gongzhengz@gmail.com>
2017-04-11 15:31:34 -0700

    4.4-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
+import data from '../testData';
 
 // extends React component
 // Only use this syntax if you need to use state.
 class App extends React.Component {
     state = {
-        pageHeader: 'Name contests.'
+        pageHeader: 'Name contests.',
+        contests:[]
     };
 
     componentDidMount(){
-
+        // after app get mounted, we will get the data.
+        this.setState({
+            contests: data.contests
+        });
     }
 
     componentWillUnmount(){
@@ -22,8 +27,10 @@ class App extends React.Component {
             <div className = 'App'>
                 <Header message={this.state.pageHeader} />
                 <div>
-                    {this.props.contests.map(contest =>
-                        <ContestPreview {...contest} />
+                    {this.state.contests.map(contest =>
+                    // For every map loop, you need to 
+                    // provide an unique key for each child element by id.
+                        <ContestPreview key={contest.id} {...contest} />
                     )}                    
                 </div>
             </div>            
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import data from './testData';
+// Eventually we have to grap data from API.
+// so we assume we don't have data yet.
+// import data from './testData';
 
 ReactDOM.render(
-    <App contests={data.contests}/>,
+    <App contests={[]} />,
     document.getElementById('root')
 );
 
