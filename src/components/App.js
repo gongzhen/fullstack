8b7e063ca7a74d58ca2c27ee999a69f3c9e1e296
gongzhen <gongzhengz@gmail.com>
2017-04-11 12:00:47 -0700

    3.5-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
-    return(
-        <div>
-            <Header message = "Header Naming Contests" />
-            <div>
-                ...
-            </div>
-        </div>
-    );};
+// extends React component
+// Only use this syntax if you need to use state.
+class App extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {test: 42};
+    // }
+
+    // same as the constructor wat.
+    state = {
+        pageHeader: 'Name contest with state.'
+    };
+    
+    render(){
+        return(
+            <div className = 'App'>
+                <Header message={this.state.pageHeader}/>
+                <div>
+                    ...                    
+                </div>
+            </div>            
+        );
+    }
+}
 
 export default App;
