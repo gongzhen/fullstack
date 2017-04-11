dc72c79d752e4733ffc18079d62aa23d28c81340
gongzhen <gongzhengz@gmail.com>
2017-04-11 14:49:36 -0700

    4.2-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 // extends React component
 // Only use this syntax if you need to use state.
 class App extends React.Component {
     state = {
-        pageHeader: 'Name contest with state.'
+        pageHeader: 'Name contests.'
     };
 
-    componentWillMount() {
-        console.log('will Mount');
-        debugger;
-    }
     componentDidMount(){
-        console.log('did Mount');
-        debugger;
+
     }
 
     componentWillUnmount(){
-        console.log('did UnMount');
-        debugger;        
+
     }
     
     render(){
         return(
             <div className = 'App'>
-                <Header message={this.state.pageHeader}/>
+                <Header message={this.state.pageHeader} />
                 <div>
-                    ...                    
+                    {this.props.contests.map(contest =>
+                        <ContestPreview {...contest} />
+                    )}                    
                 </div>
             </div>            
         );
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     module: {
         loaders: [
             {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
                 test: /\.js$/,
                 loader: 'babel-loader'
             }
