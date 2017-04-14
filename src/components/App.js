cb73846601c219b1105e8486dd1a0b551191e21e
gongzhen <gongzhengz@gmail.com>
2017-04-14 11:53:23 -0700

    6.4-end A bit of refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
 import Contest from './Contest';
+import * as api from '../api';
 
 const pushState = (obj, url) => window.history.pushState(obj, '', url);
 
@@ -24,18 +25,21 @@ class App extends React.Component {
             { currentContestId: contestId },
             `/contest/${contestId}`
         );
-
-        // lookup the contest
-        // this.state.contests[contestId]
-        this.setState({
-        pageHeader: this.state.contests[contestId].contestName,
-        currentContestId: contestId
-        });        
+        api.fetchContest(contestId).then(contest=> {
+            this.setState({
+                pageHeader: contest.contestName,
+                currentContestId: contest.id,
+                contests:{
+                    ...this.state.contests,
+                    [contest.id]: contest
+                }
+            });                    
+        });
     }
 
   currentContent() {
     if (this.state.currentContestId) {
-      return <Contest {...this.state.contests[this.state.currentContestId]} />;
+        return <Contest {...this.state.contests[this.state.currentContestId]} />;
     }
 
     return <ContestList
